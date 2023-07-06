5a3917b0745d1a1ac3b4139aac9385ed0c06a8ce
yohqin <15927700473@163.com>
2023-07-06 23:38:08 +0800

    add user-profile

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,6 +1,20 @@
 <template>
     <view>
-        my
+        <view class="p-16 flex items-center" @click="onToUserInfoPage">
+            <u-image
+                src="https://cdn.uviewui.com/uview/album/1.jpg"
+                width="48px"
+                height="48px"
+                shape="circle"
+            />
+            <view class="flex-1 pl-16 flex flex-column justify-between">
+                <view class="font-size-16 font-bold">为你存在</view>
+                <view class="font-size-12">当前等级：2</view>
+            </view>
+            <view>
+                <u-icon name="arrow-right"></u-icon>
+            </view>
+        </view>
     </view>
 </template>
 
@@ -12,6 +26,13 @@ export default {
           page.getTabBar().setData({
             selected: 3
           })
+        }
+    },
+    methods: {
+        onToUserInfoPage () {
+            wx.navigateTo({
+                url: '/packageA/pages/user-profile/user-profile'
+            })
         }
     }
 }
